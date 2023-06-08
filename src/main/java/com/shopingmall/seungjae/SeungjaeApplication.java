a9455a0120d914bcd96635cedf562d712fd509d9
seungjaejeon <wjstmdwo2000@naver.com>
2023-06-08 11:24:28 +0900

    generated

---
+++ src/main/java/com/shopingmall/seungjae/SeungjaeApplication.java
@@ -0,0 +1,13 @@
+package com.shopingmall.seungjae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeungjaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeungjaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopingmall/seungjae/SeungjaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopingmall.seungjae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeungjaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
