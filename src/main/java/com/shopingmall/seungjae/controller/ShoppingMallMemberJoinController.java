f579e39f507305af32186968a15d2d80524aa3f0
seungjaejeon <wjstmdwo2000@naver.com>
2023-07-03 15:40:44 +0900

    bindingResult

--- src/main/java/com/shopingmall/seungjae/controller/ShoppingMallMemberJoinController.java
+++ src/main/java/com/shopingmall/seungjae/controller/ShoppingMallMemberJoinController.java
@@ -7,6 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -23,22 +25,19 @@ public class ShoppingMallMemberJoinController {
     }
 
     @PostMapping
-    public String CreateMember(@ModelAttribute Member member, Model model){
+    public String CreateMember(@ModelAttribute Member member, BindingResult bindingResult){
         //검증
-        Map<String, String> errors = new HashMap<>();
         if(!StringUtils.hasText(member.getName())){
-            errors.put("name", "문자를 적으셔야 합니다.");
+            bindingResult.addError(new FieldError("member", "name", "문자를 적으셔야 합니다."));
         }
         if(!StringUtils.hasText(member.getLoginId())||member.getLoginId().length()<3||member.getLoginId().length()>10){
-            errors.put("loginId", "올바르지 않은 ID: 문자가 있어야하고 아이디의 길이는 3자 이상 10자 이하여야 합니다.");
+            bindingResult.addError(new FieldError("member", "loginId", "올바르지 않은 ID: 문자가 있어야하고 아이디의 길이는 3자 이상 10자 이하여야 합니다."));
         }
         if(member.getPassword().length()<3||member.getPassword().length()>10){
-            errors.put("password", "올바르지 않은 PASSWORD: 비밀번호의 길이는 3자 이상 10자 이하여야 합니다.");
+            bindingResult.addError(new FieldError("member", "password", "올바르지 않은 PASSWORD: 비밀번호의 길이는 3자 이상 10자 이하여야 합니다."));
         }
-        model.addAttribute("member", member);
-        if(!errors.isEmpty()){
-            log.info("errors = {}", errors);
-            model.addAttribute("errors", errors);
+        if(bindingResult.hasErrors()){
+            log.info("bindingResult = {}", bindingResult);
             return "join/joinPage";
         }
         memberService.join(member);
