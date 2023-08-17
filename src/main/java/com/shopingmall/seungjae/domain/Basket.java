c7928460078da5fd92829d7dcbcbd0643458846f
seungjaejeon <wjstmdwo2000@naver.com>
2023-08-17 17:38:15 +0900

    readme update

--- src/main/java/com/shopingmall/seungjae/domain/Basket.java
+++ src/main/java/com/shopingmall/seungjae/domain/Basket.java
@@ -17,7 +17,8 @@ public class Basket {
     private Member member;
     @OneToMany(mappedBy = "basket")
     private List<BasketItem> basketItems = new ArrayList<>();
-    public static Basket createBasket(Member member) { // 장바구니 생성
+    public static Basket createBasket(Member member) { // 장바구니를 생성하는 정적 메서드입니다.
+        // Member를 인자로 받아 새로운 Basket 엔티티를 생성하고 member를 설정하여 반환합니다.
         Basket basket = new Basket();
         basket.setMember(member);
         return basket;
@@ -29,3 +30,5 @@ public class Basket {
                 '}';
     }
 }
+//이 클래스는 장바구니를 나타내며, 회원(Member)과 장바구니 항목(BasketItem) 간의 관계를 맺는 역할을 수행합니다.
+// member 필드로 장바구니를 생성한 회원과 연결하며, basketItems 필드로 장바구니에 속한 항목들을 관리합니다.
